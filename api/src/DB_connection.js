750c91e2da58441086b59bbf7cc59377a61db9be
braiaguirre <brianaguirre.sp94@gmail.com>
2023-09-23 11:22:20 -0300

    update

---
+++ api/src/DB_connection.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+// const modelUser = require('./models/User');
+// const modelFavorite = require('./models/Favorite');
+
+const sequelize = new Sequelize(
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/pokemon`,
+   { logging: false, native: false }
+);
+
+// modelUser(sequelize);
+// modelFavorite(sequelize);
+
+// const { User, Favorite } = sequelize.models;
+// User.belongsToMany(Favorite, {through: 'user_favorite'});
+// Favorite.belongsToMany(User, {through: 'user_favorite'});
+
+module.exports = {
+   // ...sequelize.models,
+   conn: sequelize,
+};
---
+++ api/src/app.js
@@ -0,0 +1,13 @@
+const morgan = require('morgan');
+const express = require('express');
+const cors = require('cors');
+const mainRouter = require('./routes/index');
+
+const server = express();
+
+server.use(express.json());
+server.use(morgan('dev'));
+server.use(cors());
+server.use('/pokemon', mainRouter);
+
+module.exports = server;
