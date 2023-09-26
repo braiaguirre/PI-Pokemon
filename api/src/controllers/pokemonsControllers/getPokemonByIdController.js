d85f90c7ee364ca7263a1bdad04671b843b451f8
braiaguirre <brianaguirre.sp94@gmail.com>
2023-09-26 10:57:18 -0300

    update

--- api/src/controllers/pokemonsControllers/getPokemonByIdController.js
+++ api/src/controllers/pokemonsControllers/getPokemonByIdController.js
@@ -1,13 +1,18 @@
 // DEPENDENCIES
 const axios = require('axios');
 const filterPokemonData = require('../../utils/filterPokemonData');
+const { Pokemon } = require('../../DB_connection');
 
 const URL = 'https://pokeapi.co/api/v2/pokemon/';
 
 const getPokemonsByIdController = async (id) => {
-    const { data } = await axios.get(`${URL}${id}`);
-    const pokemon = filterPokemonData(data);
-    return pokemon;
+    const pokemonDb = await Pokemon.findOne({ where: { id } });
+    if (!pokemonDb) {
+        const { data } = await axios.get(`${URL}${id}`);
+        const pokemonApi = filterPokemonData(data);
+        return pokemonApi;
+    }
+    return pokemonDb;
 }
 
 module.exports = getPokemonsByIdController;
